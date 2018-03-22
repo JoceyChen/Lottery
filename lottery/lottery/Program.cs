18bbc00373de16f265f77dd10a1b29516d4cd334
Jocey <copy15678@gmail.com>
2018-03-22 15:49:36 +0800

    亂數

---
+++ lottery/lottery/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //創建亂數
+            Random rand = new Random();
+
+            //顯示亂數
+            Console.WriteLine(rand.Next(0, 100));
+
+            //結束
+            Console.ReadLine();
+        }
+    }
+}
