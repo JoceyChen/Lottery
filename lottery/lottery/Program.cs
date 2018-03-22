fd8e8f1633988a7e55c8fff5332ae326bbf50139
Jocey <copy15678@gmail.com>
2018-03-22 16:54:32 +0800

    產生10組亂數

--- lottery/lottery/Program.cs
+++ lottery/lottery/Program.cs
@@ -13,15 +13,22 @@ namespace lottery
             // 產生亂數
             Random rand = new Random();
 
-            int n1 = rand.Next(1, 43);
-            int n2 = rand.Next(1, 43);
-            int n3 = rand.Next(1, 43);
-            int n4 = rand.Next(1, 43);
-            int n5 = rand.Next(1, 43);
-            int n6 = rand.Next(1, 43);
+            int i = 1;
+                                    
+            while (i<11)
+            {
+                int n1 = rand.Next(1, 43);
+                int n2 = rand.Next(1, 43);
+                int n3 = rand.Next(1, 43);
+                int n4 = rand.Next(1, 43);
+                int n5 = rand.Next(1, 43);
+                int n6 = rand.Next(1, 43);
+                
+                i++;
 
-            // 顯示亂數
-            Console.WriteLine("號碼:{0:00} {1:00} {2:00} {3:00} {4:00} {5:00}", n1, n2, n3, n4, n5, n6);
+                // 顯示亂數
+                Console.WriteLine("{0:00} {1:00} {2:00} {3:00} {4:00} {5:00} {6:00}", n1, n2, n3, n4, n5, n6 + "\n");
+            }
 
             // 暫停並顯示
             Console.ReadLine();
